66a054fef1c3f08193b0d9f7e4186223ea977e80
Tsuki0715 <108515571+Tsuki0715@users.noreply.github.com>
2023-01-15 10:04:58 +0800

    Add files via upload

---
+++ style/infopage.js
@@ -0,0 +1,9 @@
+function home(){
+    //window.location.href = "http://www.w3schools.com";
+    //alert("Success");
+    //document.getElementsByClassName("turnPage").value = "abc";
+}
+
+function timetable() {
+    document.getElementById("picTime").src = "";
+}
