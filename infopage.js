2e3ea9c297d7968b025797f1270eefd57378cef5
Tsuki0715 <108515571+Tsuki0715@users.noreply.github.com>
2023-01-15 10:06:25 +0800

    Delete infopage.js

--- infopage.js
+++
@@ -1,9 +0,0 @@
-function home(){
-    //window.location.href = "http://www.w3schools.com";
-    //alert("Success");
-    //document.getElementsByClassName("turnPage").value = "abc";
-}
-
-function timetable() {
-    document.getElementById("picTime").src = "";
-}
